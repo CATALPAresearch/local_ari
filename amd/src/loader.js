d6a21880491e7d7315d8b99013d3c3f1fbe2ea2d
Marc Burchart <marc.burchart@fernuni-hagen.de>
2020-03-09 16:13:39 +0100

    erased every ts class and started new. fixed the bug of not getting loaded.

--- amd/src/loader.js
+++ amd/src/loader.js
@@ -3,16 +3,12 @@
  * @version 1.0.0
  * @description The Controller
  */
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-define(["require", "exports", "./controller"], function (require, exports, controller_1) {
+define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    controller_1 = __importDefault(controller_1);
     function init() {
         try {
-            new controller_1.default();
+            alert("Hier spielt die Musik!");
         }
         catch (error) {
             console.log(error);
