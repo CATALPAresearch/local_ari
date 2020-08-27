5c753e78e2d966347ad896a1e71f9d4208821a3c
Marc <marc.burchart@fernuni-hagen.de>
2020-08-27 11:01:42 +0200

    added uniqid and sensor_tab

---
+++ amd/tsc/sensor_tab.ts
@@ -0,0 +1,18 @@
+import { SessionStorage } from "./core_storage";
+import { uniqid } from "./core_helper";
+
+/**
+ * 
+ * @author Marc Burchart
+ * @version 1.0-20200827
+ * @description Get the uniqid of the Tab.
+ * 
+ */
+
+export function getTabID():string{
+    let old = SessionStorage.get("uniqid");
+    if(old !== null && typeof old === "string" && old.length > 0) return old;
+    let newID = uniqid();
+    SessionStorage.set("uniqid", newID, true);
+    return newID;
+}
