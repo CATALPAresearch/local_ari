ec267b4948297f81044a1a8e0a88eb4444f8b655
Marc <marc.burchart@fernuni-hagen.de>
2020-09-02 16:34:13 +0200

    cleaned version

--- amd/tsc/controller.ts
+++ amd/tsc/controller.ts
@@ -1,6 +1,8 @@
 //import { ServiceWorker } from './core_worker';
 //import { LearnerModelManager } from './learner_model_manager';
-import { Webcam } from './core_webcam';
+
+import { LearnerModelManager } from "./learner_model_manager";
+
 /**
  * 
  * @author Marc Burchart
@@ -13,11 +15,10 @@ export class Controller{
   
   private wwwroot:string;
 
-  constructor(wwwroot: string) {
-    //new LearnerModelManager();
+  constructor(wwwroot: string) {  
+    new LearnerModelManager(); 
     this.wwwroot = wwwroot;
-    console.log(this.wwwroot);
-    new Webcam();
+    console.log(this.wwwroot);    
   }
     
 }
