a934caf9e8d551cd6384db6a50a6a392ea492e28
ChiaraSand <40384044+ChiaraSand@users.noreply.github.com>
2022-11-25 16:08:51 +0100

    updated view.php

--- view.php
+++ view.php
@@ -3,34 +3,20 @@
 require('../../config.php');
 require_once('lib.php');
 
-
+$title = get_string('pluginname', 'local_ari');
 $PAGE->set_context(context_system::instance());
-$PAGE->set_title(get_string('pluginname', 'local_ari'));
+$PAGE->set_title($title);
+$PAGE->set_heading($title);
 //$PAGE->set_url(new moodle_url('/local_ari'));
 //$PAGE->requires->css(new moodle_url('/local/local_ari/styles.css'));
 $PAGE->requires->js_call_amd('local_ari/app-lazy', 'init');
-//$PAGE->requires->js_call_amd('local_ari/main', 'init');
 $PAGE->navbar->add(get_string('pluginname', 'local_ari'));
-
-
-//$pdata = strtolower(basename(dirname(__DIR__)).'_'.basename(__DIR__)).'\core\pinfo';
-//$pdata = new $pdata();
-//$pdata = $pdata->getData();
-
-//$url = new moodle_url("{$pdata->moodlePath}/view.php");
-//$PAGE->set_url($url);
-//$PAGE->requires->js_call_amd("local_ari/loader", "init");  //array($CFG->wwwroot)
-
 
 
 echo $OUTPUT->header();
 
 echo <<<'EOT'
-<h1>ARI - Test</h1>
-<div id="app">
-    <span>vue</span>
-    <router-view></router-view>
-</div>
+<div id="app"></div>
 EOT;
 
 echo $OUTPUT->footer();
