f147f425a86f86e22fc6cadd8485c9f5445614ca
ChiaraSand <40384044+ChiaraSand@users.noreply.github.com>
2022-12-15 15:13:19 +0100

    init communication to db

---
+++ classes/external/rule.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package     local_ari
+ * @copyright   2022 Chiara Sandführ <chiara.sandfuehr@fernuni-hagen.de>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_ari\external;
+
+use external_api;
+use external_function_parameters;
+use external_multiple_structure;
+use external_settings;
+use external_value;
+use external_single_structure;
+use required_capability_exception;
+use stdClass;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir.'/externallib.php');
+
+/**
+ * Class rule
+ *
+ * @package     local_ari
+ * @copyright   2022 Chiara Sandführ <chiara.sandfuehr@fernuni-hagen.de>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class rule extends external_api {
+}
+
--- vue/src/main.ts
+++ vue/src/main.ts
@@ -2,10 +2,11 @@ import Vue from "vue";
 // import VueRouter from "vue-router";
 //import { store } from "./store";
 import App from "./app.vue";
-// import Communication from "./scripts/communication";
+import Communication from "../scripts/communication";
 
 function init() {
     // __webpack_public_path__ = M.cfg.wwwroot + "/local/ari/build/";
+    Communication.setPluginName("local_ari");
 
     new Vue({
         el: "#app",
