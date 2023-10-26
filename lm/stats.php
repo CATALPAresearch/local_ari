be9493195dfda43cae4b99c6d375b7f8fac850b2
nise <niels.seidel@nise81.com>
2023-10-26 08:10:21 +0200

    structured LM statistics

---
+++ lm/stats.php
@@ -0,0 +1,24 @@
+<?php
+
+# compute and store date for one course
+if(isset($REQUEST['compute_data_of_course'])){
+    # check if data for this course was already collected
+    
+    # get all user for a course
+
+    # get LM for each user and week
+    foreach($users as $user){
+        foreach($weeks as $week){
+            # make LM flat
+
+            # store flat data: user, week, key, value
+
+        }
+    }    
+}
+
+# requst data for visualization
+if(isset($REQUEST['get_data_of_course'])){
+
+}
+
